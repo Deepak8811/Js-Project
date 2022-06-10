3f6c0713fbf84c3a8faf70e7ef467bb8da4356b6
Deepak <deepchaurasiya1997@gmail.com>
2022-06-10 05:39:25 +0530

    first commit

--- library.js
+++ library.js
@@ -8,31 +8,49 @@ function Book(name,author,type){
 
 }
 //Display constructor
-function Display(){
+function Display() {
 
 }
 //Add method to display prototype
 Display.prototype.add = function(Book){
     console.log("Adding to UI");
     tableBody = document.getElementById('tableBody')
-    let uiString = `
-    
-    
-    `
+    let uiString = `<tr>
+                       <th scope="row">1</th>
+                       <td>${Book.name}</td>
+                       <td>${Book.author}</td>
+                       <td>${Book.type}</td>
+                    </tr>  `;
 
+    tableBody.innerHTML += uiString;
 }
+//Implement the clear function
 Display.prototype.clear = function(){
     let libraryForm = document.getElementById('libraryForm');
     libraryForm.reset();
     
 }
+//Implement the valid  function
+Display.prototype.validate = function(book){
+  if(book.name.length<2 || book.author.length<2) {
+      return false;
 
+  }
+  else{
+      return true;
+  }
+    
+}
+Display.prototype.show = function(type, displayMessage){
+   let message =  document.getElementById('message');
+   message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                          <strong>${displayMessage}</strong> You should check in on some of those fields below.
+                          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>`
 
-
-
-
-
-
+}
 
 //Add submirt event listioner to libraryForm
 let libraryForm = document.getElementById('libraryForm');
@@ -68,10 +86,14 @@ function libraryFormSubmit(e){
     let book = new Book(name, author,type);
     console.log(Book);
 let display = new Display();
-display.add(book);
-display.clear();
-
-
-    
+if(display.validate(book)){
+    display.add(book);
+    display.clear();
+    display.show('Success','Your book has been successfully added.');
+}
+else{
+    //Show error to the user.
+    display.show('Danger','Sorry you can not add this book.');
+}
 
 }
