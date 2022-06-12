0c6c5068ebc5bf9b88a369702994f33d44d38816
Deepak <deepchaurasiya1997@gmail.com>
2022-06-12 14:02:03 +0530

    initial commit

--- library.js
+++ library.js
@@ -1,99 +1,101 @@
-console.log("This is library in javaScript");
-//constructor
-function Book(name,author,type){
+console.log("This is index.js");
+// Todos"
+// 1. Store all the data to the localStorage
+// 2. Give another column as an option to delete the book
+// 3. Add a scroll bar to the view
+
+// Constructor
+function Book(name, author, type) {
     this.name = name;
-    this.aurhor = author;
+    this.author = author;
     this.type = type;
-
-
 }
-//Display constructor
+
+// Display Constructor
 function Display() {
 
 }
-//Add method to display prototype
-Display.prototype.add = function(Book){
-    console.log("Adding to UI");
-    tableBody = document.getElementById('tableBody')
-    let uiString = `<tr>
-                       <th scope="row">1</th>
-                       <td>${Book.name}</td>
-                       <td>${Book.author}</td>
-                       <td>${Book.type}</td>
-                    </tr>  `;
 
+// Add methods to display prototype
+Display.prototype.add = function (book) {
+    console.log("Adding to UI");
+    tableBody = document.getElementById('tableBody');
+    let uiString = `<tr>
+                        <td>${book.name}</td>
+                        <td>${book.author}</td>
+                        <td>${book.type}</td>
+                    </tr>`;
     tableBody.innerHTML += uiString;
 }
-//Implement the clear function
-Display.prototype.clear = function(){
+
+// Implement the clear function
+Display.prototype.clear = function () {
     let libraryForm = document.getElementById('libraryForm');
     libraryForm.reset();
-    
-}
-//Implement the valid  function
-Display.prototype.validate = function(book){
-  if(book.name.length<2 || book.author.length<2) {
-      return false;
-
-  }
-  else{
-      return true;
-  }
-    
-}
-Display.prototype.show = function(type, displayMessage){
-   let message =  document.getElementById('message');
-   message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
-                          <strong>${displayMessage}</strong> You should check in on some of those fields below.
-                          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>`
-
 }
 
-//Add submirt event listioner to libraryForm
+// Implement the validate function
+Display.prototype.validate = function (book) {
+    if (book.name.length < 2 || book.author.length < 2) {
+        return false
+    }
+    else {
+        return true;
+    }
+}
+Display.prototype.show = function (type, displayMessage) {
+    let message = document.getElementById('message');
+    message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Messge:</strong> ${displayMessage}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                            </button>
+                        </div>`;
+    setTimeout(function () {
+        message.innerHTML = ''
+    }, 2000);
+
+}
+
+
+// Add submit event listener to libraryForm
 let libraryForm = document.getElementById('libraryForm');
 libraryForm.addEventListener('submit', libraryFormSubmit);
 
-
-function libraryFormSubmit(e){
-   
-    console.log('You have submitted library form');
-    e.preventDefault();
-   
-    
+function libraryFormSubmit(e) {
+    console.log('YOu have submitted library form');
     let name = document.getElementById('bookName').value;
-
     let author = document.getElementById('author').value;
     let type;
-    //development, programming, technology
-
     let development = document.getElementById('development');
     let programming = document.getElementById('programming');
     let technology = document.getElementById('technology');
-    if(development.checked){
-        type= development.value;
-    }
-    else if(programming.checked){
-        type= programming.value;
 
+    if (development.checked) {
+        type = development.value;
     }
-    else if(technology.checked){
-        type= technology.value;
-        
+    else if (programming.checked) {
+        type = programming.value;
     }
-    let book = new Book(name, author,type);
-    console.log(Book);
-let display = new Display();
-if(display.validate(book)){
-    display.add(book);
-    display.clear();
-    display.show('Success','Your book has been successfully added.');
-}
-else{
-    //Show error to the user.
-    display.show('Danger','Sorry you can not add this book.');
-}
+    else if (technology.checked) {
+        type = technology.value;
+    }
 
+    let book = new Book(name, author, type);
+    console.log(book);
+
+    let display = new Display();
+
+    if (display.validate(book)) {
+
+        display.add(book);
+        display.clear();
+        display.show('success', 'Your book has been successfully added.')
+    }
+    else {
+        // Show error to the user
+        display.show('danger', 'Sorry you cannot add this book');
+    }
+
+    e.preventDefault();
 }
